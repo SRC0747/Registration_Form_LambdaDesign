f9f863aec27239bfcedd1881845208becd8c0ae5
SRC0747 <RjSrc@0747>
2021-08-31 22:06:22 +0530

    added: Validate First Username.

--- src/test/java/com/registraion_form_design/RegistrationForm.java
+++ src/test/java/com/registraion_form_design/RegistrationForm.java
@@ -1,7 +1,47 @@
 package com.registraion_form_design;
+import java.util.regex.*;
+//import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
 
 public class RegistrationForm {
-    public static void main(String args[]){
-        System.out.println("Welcome to Registration Form Problem.");
+    / Function to validate the First Username
+    public static boolean isValidFirstUsername(String name)
+    {
+
+        // Regex to check valid username.
+        String regex = "^[Cap]\\w{5,29}[A-Za-z]$";
+
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+
+        // If the username is empty
+        // return false
+        if (name == null) {
+            return false;
+        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given username
+        // and regular expression.
+        Matcher m = p.matcher(name);
+
+        // Return if the username
+        // matched the ReGex
+        return m.matches();
+    }
+    public static void main(String[] args)
+    {
+
+        // Test Case: 1
+        String str1 = "Geeksforgeeks";
+        System.out.println(isValidFirstUsername(str1));
+
+        // Test Case: 2
+        String str2 = "Cap23aSjk";
+        System.out.println(isValidFirstUsername(str2));
+
+        // Test Case: 3
+        String str3 = "Cap47RjSrc";
+        System.out.println(isValidFirstUsername(str3));
     }
 }
