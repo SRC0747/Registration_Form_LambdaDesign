fd92c792679bf8480cbd77708c30007c4aeee7ce
SRC0747 <RjSrc@0747>
2021-08-31 22:03:08 +0530

    Welcome to  Registration Form Problem.

---
+++ src/test/java/com/registraion_form_design/RegistrationForm.java
@@ -0,0 +1,7 @@
+package com.registraion_form_design;
+
+public class RegistrationForm {
+    public static void main(String args[]){
+        System.out.println("Welcome to Registration Form Problem.");
+    }
+}
