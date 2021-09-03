d6633e0c305913ebcfecd02ccf7b6fd3ac4a3163
SRC0747 <RjSrc@0747>
2021-09-03 20:38:10 +0530

    added: Clear all the Ample Validate Email Addresses.

--- src/test/java/com/registraion_form_design/RegistrationForm.java
+++ src/test/java/com/registraion_form_design/RegistrationForm.java
@@ -123,5 +123,20 @@ public class RegistrationForm {
         //Checking at least 1 Special Character in Password.
         String password_Spceial_char = "Sampriti07@47$July";
         System.out.println(isValidPassword(password_Spceial_char));
+
+        //Remove all Sample Email-id
+        String input = "Hello world my # is 123 mail me @ test@test.com";
+        String EMAIL_PATTERN = "([^.@\\s]+)(\\.[^.@\\s]+)*@([^.@\\s]+\\.)+([^.@\\s]+)";
+
+        String output = input.replaceAll(EMAIL_PATTERN, "") // Replace emails
+                // by an empty string
+                .replaceAll("\\p{Punct}", "") // Replace all punctuation. One of
+                // !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
+                .replaceAll("\\d", "") // Replace any digit by an empty string
+                .replaceAll("\\p{Blank}{2,}+", " "); // Replace any Blank (a  space or
+        // a tab) repeated more than once
+        // by a single space.
+
+        System.out.println(output);
     }
 }
