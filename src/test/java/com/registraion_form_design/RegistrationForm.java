fcb04c67c040a162fb203185906769bc5f7008ac
SRC0747 <RjSrc@0747>
2021-09-03 21:12:33 +0530

    added: Handling Custom Exception.

--- src/test/java/com/registraion_form_design/RegistrationForm.java
+++ src/test/java/com/registraion_form_design/RegistrationForm.java
@@ -4,7 +4,54 @@ import java.util.regex.*;
 //import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 
-public class RegistrationForm {
+public class RegistrationForm extends Exception{
+
+        public RegistrationForm(String message, Throwable cause, ErrorCode code) {
+            super(message, cause);
+            this.code = code;
+        }
+        public RegistrationForm(String message, Throwable cause, ErrorCode code) {
+            super(message, cause);
+            this.code = code;
+        }
+
+        public RegistrationForm(String message, ErrorCode code) {
+            super(message);
+            this.code = code;
+        }
+
+        public RegistrationForm(Throwable cause, ErrorCode code) {
+            super(cause);
+            this.code = code;
+        }
+
+        public ErrorCode getCode() {
+            return this.code;
+        }
+        public void wrapException(String input) throws RegistrationForm {
+            try {
+                //Checking First Username.
+                String str1 = "Geeksforgeeks";
+                System.out.println(isValidFirstUsername(str1));
+
+                //Checking Last Username
+                String str3 = "Riya526Biny";
+                System.out.println(isValidLastUsername(str3));
+
+                //Checking Email-id.
+                String email = "sampritircgmail.com";
+                System.out.println(isValidEmail(email));
+
+                //Checking Valid Mobile Number.
+                System.out.println(whenMatchesTenDigitsNumber_thenCorrect());
+
+                //Checking Password.
+                String password = "srcsrkkolkata";
+                System.out.println(isValidPassword(password));
+            } catch (NumberFormatException e) {
+                throw new RegistrationForm("A message that describes the error.", e, ErrorCode.INVALID_PORT_CONFIGURATION);
+            }
+        }
     / Function to validate the First Username
     public static boolean isValidFirstUsername(String name)
     {
@@ -172,5 +219,6 @@ public class RegistrationForm {
         //Checking JUnit Parameterised Test Cases.
         RegistrationForm p_obj = new RegistrationForm();
         p_obj.parameterised_Test(String name);
+        
     }
 }
